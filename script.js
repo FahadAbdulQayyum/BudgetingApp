8144de064a2702883fc3ab1ecd840276efc06ba0
unknown <fahad.45843@iqra.edu.pk>
2023-06-01 01:14:13 +0500

    Fully developed with comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// All important ids imported
 const firstContainer = document.getElementById('first-container');
 const secondContainer = document.getElementById('second-container');
 const thirdContainer = document.getElementById('third-container');
@@ -16,79 +17,96 @@ const list = document.getElementById('list');
 const totalBudgetDisplay = document.getElementById('totalBudgetDisplay');
 const remainingBalanceDisplay = document.getElementById('remainingBalanceDisplay');
 
-
+// Form validation boolean is used
 let validate = false;
+// Total budget amount to store for display and calculating other functionalities
 let totalBudgetAmount = 0;
+// Remaining amount to store to display and calculating other functionalities
 let remainingBalanceAmount = 0;
+// Comparison used for validating the form's amount and deciding upon which to redirect or not to next page
 let compareAmount = 0;
+// Array used for collecting all objectified data
 let arr = [];
 
+// Enlisting function
 function arrayEnlist(arr){
+    // Different icons with the img src link
     const iconMug = () => '<img id="mug" src="./icons/mug.png"/>'
     const iconBag = () => '<img id="bag" src="./icons/bag.png"/>'
     const iconGift = () => '<img id="gift" src="./icons/gift.png"/>'
     const iconCard = () => '<img id="card" src="./icons/card.png"/>'
     const bookMark = () => '<img id="mark" src="./icons/mark.png"/>'
 
+    // Filtering object with positive number
     let posNum = arr.filter(v=>v.amount >= 0)
+    // Filtering object with negative number
     let negNum = arr.filter(v=>v.amount < 0)
 
+    // Mapping only the positive amount array
     let posArray = posNum.map(v=>v.amount)
+    // Mapping only the negative amount array
     let negArray = negNum.map(v=>v.amount)
     
-    console.log('posArray', posArray)
-    console.log('negArray', negArray)
-
+    // Reducing positive array mean summing them
     let posRed = posArray.reduce((a,b)=> +a + +b,0)
+    // Reding negative array mean summing them
     let negRed = negArray.reduce((a,b)=> +a + +b,0)
 
-    console.log('posRed', posRed)
-    console.log('negRed', negRed)
-
+    // Calculating the remaining amount
     remainingBalanceAmount = +totalBudgetAmount - (posRed + negRed);
 
-    // *******************
+    // Displaying the total Budget entered by user
     totalBudgetDisplay.innerHTML = "<div id = 'totalDisplay'>"+ 'Balance: ' + "<h4>" + '$' + totalBudgetAmount + "</h4>" + "</div>"
+    // Display the remaining amount after calculation
     remainingBalanceDisplay.innerHTML = "<div id = 'totalDisplay'>"+ 'Remaining: ' + "<h4>" + '$' + remainingBalanceAmount + "</h4>" + "</div>"
-    // *******************
 
+    // Emptying the 'li' to avoid dual data recycling of list
     list.innerHTML = ""
 
+    // Displaying the list using forEach
     arr.forEach((v, i)=>{
+    // Icons identified/used based on their category
     const icon = v.category === 'Restaurants & Cafe' ? iconMug() 
     : v.category === 'Clothes & Shopping' ? iconBag() 
     : v.category === 'Credit & Loans' ? iconCard()
     : v.category === 'Gifts Card' ? iconGift()
     : ''
 
+    // Creating 'li'
     let li = document.createElement('li');
+    // Appending the list each iteration
     li.innerHTML += "<div id='li'>" + "<div>" + icon + "</div>" + "<div>" + "<span>" + v.category + "</span>" + "<span>"  +  v.date + "</span>" + "</div>" + "<div id='rightmost'>" + "<span id='amountt'>" + '-$' + v.amount + "</span>" + "<span>" + bookMark() + "</span>" + "<span id='cancel' onClick='clickCancel(" + i + ")'>" + '&times' + "</span>" + "</div>" + "</div>";
+    // Appending 'li' to list 
     list.appendChild(li);
     })
 }
 
+// Function used when user click the 'Cancel' or times button
 function clickCancel(i){
-    console.log('i', i)
+    // Respective list is removed from the array and updated
     arr.splice(i, 1)
 
+    // Updated array is again enlisted by sending them to arrayEnlist function
     arrayEnlist(arr)
 }
 
+// This function is firstContainer when user enters the Budget Amount
 function submitBudget(){
+    // Assigning budget amount
     totalBudgetAmount = totalBudget.value;
-    console.log('totalBudgetAmount',totalBudgetAmount)
     
+    // Currently secondContainer is to only display
     firstContainer.style.display = 'none'
     secondContainer.style.display = 'block'
     thirdContainer.style.display = 'none'
 }
 
+// After filling the form for adding expense this function is executed
 function submitForm(e){
     e.preventDefault();
 
+    // if form's validation is true then process proceeds otherwise does not
     if(validate){
-        console.log('Yes, it is valid!')
-
         let obj = {
             amount: amount.value,
             desc: desc.value,
@@ -96,39 +114,44 @@ function submitForm(e){
             date: date.value
         }
         
-        console.log('totalBudgetAmount',totalBudgetAmount)
-        console.log('obj',obj)
-
+        // Obj is pushed to array
         arr.push(obj)
 
+        // Only thirdContainer to display
         firstContainer.style.display = 'none'
         secondContainer.style.display = 'none'
         thirdContainer.style.display = 'block'
 
+        // Enlist the array
         arrayEnlist(arr);
     }
-
-    console.log('invalidate')
 }
 
+// When user wants to add expense by clicking the button, this function executes
 function openSecondContainer(){
     amount.value = ''
     desc.value = ''
     select.value = ''
     date.value = ''
 
+    // Second Container display which has form for adding expense
     secondContainer.style.display = 'block';
 }
 
+// function for validating the form
 function validation(){
 
+    // Calling the small's parentElement
     let inp = small.parentElement;
+    // then getting the input's tag for executing some functionalities
     let input = inp.querySelector('input') 
 
+    // Initially small tag to be invisible to avoid user's distraction
     small.style.display = 'none'
 
-    console.log('remainingAmount, totalBudget', +remainingBalanceAmount>1, totalBudgetAmount)
+    // Assigning to 'compareAmount' a value by applying following's condition
     compareAmount = +remainingBalanceAmount > 1 ? +remainingBalanceAmount : +totalBudgetAmount
+    // If user enters 0
     if(+amount.value === 0){
         small.style.display = 'block'
         small.style.color = 'red'
@@ -136,13 +159,16 @@ function validation(){
         validate = false
         return small.innerText = `Input should be greater than zero.`
     }
+    // If user enter a over budget amount
     else if(+amount.value > compareAmount){
         small.style.display = 'block'
         small.style.color = 'red'
         input.style.boxShadow = '1px 1px 5px 1px red'
         validate = false
         return small.innerText = `Your input is exceeding than ${compareAmount}`
-    }else {
+    }
+    // If valid amount is entered by user
+    else {
         small.style.display = 'block'
         small.style.color = 'green'
         input.style.boxShadow = '1px 1px 5px 1px green'
@@ -151,7 +177,11 @@ function validation(){
     }
 }
 
+// firstContainer Budget amount when entered and button is pressed
 subBtnTtl.addEventListener('click', submitBudget)
+// Form's amount validation is to be performed when user inputs
 amount.addEventListener('input', validation)
+// When user submit the add expense form
 subBtnForm.addEventListener('click',submitForm)
+// When user wants to add expense by pressing button
 addAgain.addEventListener('click', openSecondContainer)
